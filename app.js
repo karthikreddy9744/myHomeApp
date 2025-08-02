fcdecc6d24b54cc82dbf2f9e824d8f8a727b8776
Karthik Reddy <23eg107c60@anurag.edu.in>
2025-08-02 11:07:48 +0530

    Switched from Jade to Pug templates

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const indexRouter = require('./routes/index');
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
